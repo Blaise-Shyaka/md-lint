b746b8c98f5c203698ac1cee6610a646ab4efe69
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2020-10-14 23:40:45 +0200

    fix bug returning nil on the line_number key of warnings hash

--- lib/line_length.rb
+++ lib/line_length.rb
@@ -2,11 +2,10 @@ class LineLength
   def line_length(file_object)
     warnings = []
     file_object.each do |line|
-      p line
       warning = {
         error_type: 'Line-length',
         error_description: 'Line is too long',
-        line_number: $INPUT_LINE_NUMBER
+        line_number: file_object.find_index(line) + 1
       }
       warnings << warning if line.length > 80
     end
