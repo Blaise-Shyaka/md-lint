380e9cff2c9ee19431a762c78fbf838bfe6bac1d
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2020-10-14 23:13:42 +0200

    add tests for the trailing_space rule

--- spec/md_lint_spec.rb
+++ spec/md_lint_spec.rb
@@ -3,17 +3,18 @@ require 'kramdown-parser-gfm'
 require_relative 'spec_helper'
 require_relative '../lib/header_rules.rb'
 require_relative '../lib/md_lint.rb'
+require_relative '../lib/trailing_space.rb'
 
 describe 'Lint' do
   let(:test_file) { Dir.glob('**/*test-md.md')[0] }
   let(:parsed_file) { Kramdown::Document.new(File.open(test_file).read, input: 'GFM').to_hash_ast }
+  let(:warning_keys) { %i[error_type error_description line_number] }
   include MdLint
 
   describe 'Header rules' do
     let(:all_headers) { all_elements_of_type(:header, parsed_file[:children]) }
     let(:misaligned_headers) { all_elements_of_type(:p, parsed_file[:children]) }
     let(:header) { HeaderRules.new }
-    let(:warning_keys) { %i[error_type error_description line_number] }
 
     describe 'Top-level header rule' do
       it 'Should return an array' do
@@ -73,6 +74,40 @@ describe 'Lint' do
         warning_hash = header.header_start_left_rule(misaligned_headers)[0]
         expect(warning_hash[:error_description]).to be_an_instance_of String
       end
+    end
+  end
+
+  describe 'Trailing space rules' do
+    let(:trailing_space) { TrailingSpace.new }
+    let(:file_object) { File.open(test_file) }
+
+    it 'Should return an array' do
+      expect(trailing_space.space_after_line(file_object)).to be_an_instance_of Array
+    end
+
+    it 'Should detect one error in the test-md.md' do
+      expect(trailing_space.space_after_line(file_object).length).to eq(1)
+    end
+
+    it 'Should return an array containing a hash with three keys' do
+      warning_hash = trailing_space.space_after_line(file_object)[0]
+      expect(warning_hash.keys).to match_array warning_keys.sort
+    end
+
+    it 'The line_number key in the warning hash should be an integer' do
+      warning_hash = trailing_space.space_after_line(file_object)[0]
+      p warning_hash
+      expect(warning_hash[:line_number]).to be_an_instance_of Integer
+    end
+
+    it 'The error_type key in the warning hash should be a string' do
+      warning_hash = trailing_space.space_after_line(file_object)[0]
+      expect(warning_hash[:error_type]).to be_an_instance_of String
+    end
+
+    it 'The error_description key in the warning hash should be a string' do
+      warning_hash = trailing_space.space_after_line(file_object)[0]
+      expect(warning_hash[:error_description]).to be_an_instance_of String
     end
   end
 end
