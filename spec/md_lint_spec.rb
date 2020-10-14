c7f0b0224008e38c5906bca7fbb89c88c1bcb6aa
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2020-10-14 22:52:40 +0200

    add tests for the header_start_left rule

--- spec/md_lint_spec.rb
+++ spec/md_lint_spec.rb
@@ -35,13 +35,43 @@ describe 'Lint' do
         expect(warning_hash[:error_type]).to be_an_instance_of String
       end
 
-      it 'The line_number key in the warning hash should be an integer' do
+      it 'The error_description key in the warning hash should be a string' do
         warning_hash = header.top_level_header(all_headers)[0]
         expect(warning_hash[:error_description]).to be_an_instance_of String
       end
 
       it 'Should highlight one line violating this rule in the test-md.md file' do
         expect(header.top_level_header(all_headers).length).to eq(1)
+      end
+    end
+
+    describe 'Header-start-left rule' do
+      it 'Should return an array' do
+        expect(header.header_start_left_rule(misaligned_headers)).to be_an_instance_of Array
+      end
+
+      it 'Should return an array containing hashes with three keys' do
+        warning_hash = header.header_start_left_rule(misaligned_headers)[0]
+        expect(warning_hash.keys).to match_array warning_keys.sort
+      end
+
+      it 'Should return an array of 3 hashes highlighting errors in the test-md.md file' do
+        expect(header.header_start_left_rule(misaligned_headers).length).to eq(3)
+      end
+
+      it 'The line_number key in the warning hash should be an integer' do
+        warning_hash = header.header_start_left_rule(misaligned_headers)[0]
+        expect(warning_hash[:line_number]).to be_an_instance_of Integer
+      end
+
+      it 'The error_type key in the warning hash should be a string' do
+        warning_hash = header.header_start_left_rule(misaligned_headers)[0]
+        expect(warning_hash[:error_type]).to be_an_instance_of String
+      end
+
+      it 'The error_description key in the warning hash should be a string' do
+        warning_hash = header.header_start_left_rule(misaligned_headers)[0]
+        expect(warning_hash[:error_description]).to be_an_instance_of String
       end
     end
   end
