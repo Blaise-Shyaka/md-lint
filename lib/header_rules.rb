59a083a4d33e672064b4c979e089b799dd12834a
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2020-10-14 03:02:56 +0200

    use guard clause rather than one-line if statement

--- lib/header_rules.rb
+++ lib/header_rules.rb
@@ -1,12 +1,12 @@
 class HeaderRules
   def top_level_header(arr_of_headers)
-    if arr_of_headers[0][:options][:level] != 1
-      {
-        error_type: 'First-header-h1',
-        error_description: 'First header should be the top-level header [h1]',
-        line_number: arr_of_headers[0][:options][:location]
-      }
-    end
+    return unless arr_of_headers[0][:options][:level] != 1
+
+    {
+      error_type: 'First-header-h1',
+      error_description: 'First header should be the top-level header [h1]',
+      line_number: arr_of_headers[0][:options][:location]
+    }
   end
 
   def header_start_left_rule(arr_misaligned_headers)
