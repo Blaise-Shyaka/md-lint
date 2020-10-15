bc3871fced29f3fdef8bb0e8ae678ff29d0e2d82
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2020-10-15 18:27:27 +0200

    delete comments in the MdLint module

--- lib/md_lint.rb
+++ lib/md_lint.rb
@@ -1,10 +1,4 @@
 module MdLint
-  # List of methods I will need
-  ## 1. Write a method to retrieve all elements of a certain type
-  ##### Takes the type of an element as the first parameter
-  ##### return an array of their options hash
-  ##### The method should not be recursive
-
   def all_elements_of_type(type, elements_list)
     all_elements = []
     elements_list.each do |el|
