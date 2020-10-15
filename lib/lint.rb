da6e48216ec93c1ce7f1e3bc6e9677de5582fca3
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2020-10-15 19:44:44 +0200

    make all_warnings helper method private

--- lib/lint.rb
+++ lib/lint.rb
@@ -3,19 +3,30 @@ require_relative './md_lint.rb'
 class Lint
   include MdLint
 
+  def print_warnings(*args)
+    all_messages = all_warnings(*args)
+    all_messages.each do |warning|
+      puts warning
+    end
+  end
+
   def files_to_lint
     return Dir.glob('**/*.md') if ARGV.length.zero?
 
     ARGV
   end
 
-  def print_warnings(*args)
+  private
+
+  def all_warnings(*args)
+    messages = []
     args.each do |arg|
       next if arg.length.zero?
 
       arg.each do |warning|
-        puts "#{warning[:error_type]} on line #{warning[:line_number]}: #{warning[:error_description]}"
+        messages << "#{warning[:error_type]} on line #{warning[:line_number]}: #{warning[:error_description]}"
       end
     end
+    messages
   end
 end
